287178dcc3da88228f7131c2cb993f63bdb060fb
Leopoldo Garcia <legarbo@gmail.com>
2013-11-26 23:00:13 -0430

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Junkcarsfortopcash::Application.config.secret_key_base = '48103a9679545f9e50db2bedbcd4f2d3290b79e218125250eb06cd1169b283154945e2122f94cb916a51f6ea4c05047e141b93c84fc65bb69391c5549ab52ccd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Junkcarsfortopcash::Application.config.session_store :cookie_store, key: '_junkcarsfortopcash_session'
